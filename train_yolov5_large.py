904a1f0c83b21916bf30fac9e2eab909df582dc8
Chaitanya-1204 <chaitanyapatil1204@gmail.com>
2025-07-20 12:41:09 +0530

    Done with 3 experimentation and started 4 more experimentation on yolov5 models

---
+++ train_yolov5_large.py
@@ -0,0 +1,162 @@
+# Training on multiple GPU's
+
+import os
+
+# Set environment variable for visible devices
+os.environ["CUDA_VISIBLE_DEVICES"] = "0 , 1"
+
+# Change to yolov5 directory
+os.chdir("yolov5")
+
+# Construct the DDP command
+command = (
+    "python -m torch.distributed.run --nproc_per_node=2 --master_port=12345 train.py "
+    "--img 640 "
+    "--batch 64 "
+    "--epochs 200 "
+    "--data ../data/data.yaml "
+    "--weights yolov5l.pt "
+    "--name yolov5_large_epochs_200_lr_0.001_ "
+    "--optimizer AdamW "
+    "--workers 4 "
+    "--project ../logs "
+    "--patience 7 "
+    "--hyp ../hyp-fine-tune.yaml "
+    "--device 0,1 "
+    "--seed 42 "
+)
+
+# Execute training
+os.system(command)
+
+
+
+# Construct the DDP command
+command = (
+    "python -m torch.distributed.run --nproc_per_node=2 --master_port=12345 train.py "
+    "--img 640 "
+    "--batch 64 "
+    "--epochs 200 "
+   "--data ../data/data.yaml "
+    "--weights yolov5l.pt "
+    "--name yolov5_large_epochs_200_lr_0.0005_ "
+    "--optimizer AdamW "
+    "--workers 4 "
+    "--project ../logs "
+    "--patience 7 "
+    "--hyp ../hyp-fine-tune_with_lr_0.0005.yaml "
+    "--device 0,1 "
+    "--seed 42 "
+)
+
+# Execute training
+os.system(command)
+
+
+
+# # Training on single GPU 
+# import os
+# os.chdir("yolov5")
+
+# os.system(
+#     "python train.py "
+#     "--img 640 "
+#     "--batch 32 "
+#     "--epochs 100 "
+#     "--data ../data/data.yaml "
+#     "--weights yolov5s.pt "
+#     "--name yolov5_small "
+#     "--optimizer AdamW "
+#     "--device 3 "
+#     "--workers 4 "
+#     "--project ../logs "
+#     "--patience 7 "  
+#     "--hyp ../hyp-fine-tune.yaml "
+#     "--seed 42 "
+    
+# )
+
+# os.system(
+#     "python train.py "
+#     "--img 640 "
+#     "--batch 32 "
+#     "--epochs 100 "
+#     "--data ../data/data.yaml "
+#     "--weights yolov5m.pt "
+#     "--name yolov5_medium "
+#     "--optimizer AdamW "
+#     "--device 3 "
+#     "--workers 4 "
+#     "--project ../logs "
+#     "--patience 7 "  
+#     "--seed 42 "
+#     "--hyp ../hyp-fine-tune.yaml"
+# )
+# os.system(
+#     "python train.py "
+#     "--img 640 "
+#     "--batch 32 "
+#     "--epochs 100 "
+#     "--data ../data/data.yaml "
+#     "--weights yolov5l.pt "
+#     "--name yolov5_large "
+#     "--optimizer AdamW "
+#     "--device 3 "
+#     "--workers 4 "
+#     "--project ../logs "
+#     "--patience 7 "  
+#     "--seed 42 "
+#     "--hyp ../hyp-fine-tune.yaml"
+# )
+
+
+# os.system(
+#     "python train.py "
+#     "--img 640 "
+#     "--batch 32 "
+#     "--epochs 100 "
+#     "--data ../data/data.yaml "
+#     "--weights yolov5s.pt "
+#     "--name yolov5_small_lr_0005 "
+#     "--optimizer AdamW "
+#     "--device 3 "
+#     "--workers 4 "
+#     "--project ../logs "
+#     "--patience 7 "
+#     "--hyp ../hyp-fine-tune_with_lr_0.0005.yaml "
+#     "--seed 42 "
+# )
+
+# os.system(
+#     "python train.py "
+#     "--img 640 "
+#     "--batch 32 "
+#     "--epochs 100 "
+#     "--data ../data/data.yaml "
+#     "--weights yolov5m.pt "
+#     "--name yolov5_medium_lr_0005 "
+#     "--optimizer AdamW "
+#     "--device 3 "
+#     "--workers 4 "
+#     "--project ../logs "
+#     "--patience 7 "
+#     "--hyp ../hyp-fine-tune_with_lr_0.0005.yaml "
+#     "--seed 42 "
+# )
+
+# os.system(
+#     "python train.py "
+#     "--img 640 "
+#     "--batch 32 "
+#     "--epochs 100 "
+#     "--data ../data/data.yaml "
+#     "--weights yolov5l.pt "
+#     "--name yolov5_large_lr_0005 "
+#     "--optimizer AdamW "
+#     "--device 3 "
+#     "--workers 4 "
+#     "--project ../logs "
+#     "--patience 7 "
+#     "--hyp ../hyp-fine-tune_with_lr_0.0005.yaml "
+#     "--seed 42 "
+# )
---
+++ train_yolov5_small_medium.py
@@ -0,0 +1,76 @@
+
+# Training on single GPU 
+import os
+os.chdir("yolov5")
+
+# os.system(
+#     "python train.py "
+#     "--img 640 "
+#     "--batch 32 "
+#     "--epochs 100 "
+#     "--data ../data/data.yaml "
+#     "--weights yolov5s.pt "
+#     "--name yolov5_small "
+#     "--optimizer AdamW "
+#     "--device 3 "
+#     "--workers 4 "
+#     "--project ../logs "
+#     "--patience 7 "  
+#     "--hyp ../hyp-fine-tune.yaml "
+#     "--seed 42 "
+    
+# )
+
+# os.system(
+#     "python train.py "
+#     "--img 640 "
+#     "--batch 32 "
+#     "--epochs 100 "
+#     "--data ../data/data.yaml "
+#     "--weights yolov5m.pt "
+#     "--name yolov5_medium "
+#     "--optimizer AdamW "
+#     "--device 3 "
+#     "--workers 4 "
+#     "--project ../logs "
+#     "--patience 7 "  
+#     "--seed 42 "
+#     "--hyp ../hyp-fine-tune.yaml"
+# )
+
+
+
+os.system(
+    "python train.py "
+    "--img 640 "
+   "--batch 64 "
+    "--epochs 200 "
+    "--data ../data/data.yaml "
+    "--weights yolov5s.pt "
+    "--name yolov5_small_lr_0005_epochs_200 "
+    "--optimizer AdamW "
+    "--device 3 "
+    "--workers 4 "
+    "--project ../logs "
+    "--patience 7 "
+    "--hyp ../hyp-fine-tune_with_lr_0.0005.yaml "
+    "--seed 42 "
+)
+
+os.system(
+    "python train.py "
+    "--img 640 "
+    "--batch 32 "
+    "--epochs 100 "
+    "--data ../data/data.yaml "
+    "--weights yolov5m.pt "
+    "--name yolov5_medium_lr_0005_epochs_200 "
+    "--optimizer AdamW "
+    "--device 3 "
+    "--workers 4 "
+    "--project ../logs "
+    "--patience 7 "
+    "--hyp ../hyp-fine-tune_with_lr_0.0005.yaml "
+    "--seed 42 "
+)
+
