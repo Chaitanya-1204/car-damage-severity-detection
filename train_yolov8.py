c919212e8e2a9c5957c2a84b5e297b5ef4edb007
Chaitanya-1204 <chaitanyapatil1204@gmail.com>
2025-07-20 14:24:35 +0530

    Added code for some new experiments will run this

---
+++ train_yolov8.py
@@ -0,0 +1,82 @@
+from ultralytics import YOLO
+
+# Load a pretrained YOLOv8 model
+model = YOLO('yolov8x.pt') 
+
+# Train the model
+model.train(
+    data='data/data.yaml',
+    epochs=200,
+    imgsz=640,
+    batch=32,
+    device='0 , 1',           
+    workers=4,
+    project='logs/',
+    name='yolov8-extra-large-lr5e-4-epochs-200',
+    lr0=0.0005,
+    optimizer='AdamW',        
+    cos_lr=True,            
+    amp=True ,
+    patience=10                
+)
+
+
+# Load a pretrained YOLOv8 model
+model = YOLO('yolov8x.pt') 
+
+# Train the model
+model.train(
+    data='data/data.yaml',
+    epochs=200,
+    imgsz=640,
+    batch=32,
+    device='0 , 1',           
+    workers=4,
+    project='logs/',
+    name='yolov8-extra-large-lr-1e-3-epochs-200',
+    lr0=0.001,
+    optimizer='AdamW',        
+    cos_lr=True,            
+    amp=True ,
+    patience=10 ,               
+)
+
+# Load a pretrained YOLOv8 model
+model = YOLO('yolov8l.pt') 
+
+# Train the model
+model.train(
+    data='data/data.yaml',
+    epochs=200,
+    imgsz=640,
+    batch=32,
+    device='0 , 1',           
+    workers=4,
+    project='logs/',
+    name='yolov8-large-lr5e-4-epochs-200',
+    lr0=0.0005,
+    optimizer='AdamW',        
+    cos_lr=True,            
+    amp=True,
+    patience=10                
+)
+
+# Load a pretrained YOLOv8 model
+model = YOLO('yolov8l.pt') 
+
+# Train the model
+model.train(
+    data='data/data.yaml',
+    epochs=200,
+    imgsz=640,
+    batch=32,
+    device='0 , 1',           
+    workers=4,
+    project='logs/',
+    name='yolov8-large-lr-1e-3-epochs-200',
+    lr0=0.001,
+    optimizer='AdamW',        
+    cos_lr=True,            
+    amp=True,
+    patience=10       
+)
